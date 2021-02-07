aa887c12d1de55adcae6f99d2a68f81f4484cd82
Your Name <you@example.com>
2021-02-07 13:40:14 +0600

    Complite with  Not founding  Massage

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const search = document.getElementById('search')
 submit = document.getElementById('submit')
 random = document.getElementById('random')
 mealsElement = document.getElementById('meals')
-resultHeading = document.getElementById('result-heasing')
+resultHeading = document.getElementById('result-heading')
 singleMealElement = document.getElementById('single-meal')
 
-
+//Search Meals Function
 function searchMeals(e) {
     e.preventDefault()
 
@@ -17,12 +17,29 @@ function searchMeals(e) {
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            resultHeading.innerHTML = `<h2>Search result '${term}':</h2>`
+
+            if (data.meals === null) {
+                resultHeading.innerHTML = '<h1>There are no result, Please try again!</h1>'
+           resultHeading.style.color = "red";
+            }else{
+                mealsElement.innerHTML = data.meals.map(meal =>`
+                    <div class = "meal">
+                    <img src = "${meal.strMealThumb}" alt = "${meal.strMeal}"/>
+                    <div class ="meal-info" data-mealID=${meal.idMeal}">
+                       <h3>${meal.strMeal}</h3> 
+                    </div>
+                    </div>
+                `)
+                .join('')
+            }
         })
+       search.value=''
     }else{
-        alert('Please search')
+        alert('Please search any meals or foods ')
     }
           
 }
 
 //Event Listener
-submit.addEventListener('submit', searchMeals)
+submit.addEventListener('submit', searchMeals) 
